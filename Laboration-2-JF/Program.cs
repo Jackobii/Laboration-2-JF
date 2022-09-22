000f806c72612497a6f70e23972eaace3e71d6b6
Jackobii <jacob.fjellstrom@iths.se>
2022-09-22 11:23:00 +0200

    Add project files.

---
+++ Laboration-2-JF/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Laboration_2_JF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
